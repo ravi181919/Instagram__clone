e193a3852ff71fbbf6b6fe241defd80b5ab69a3b
ravigupta <rg187888@gmail.com>
2024-06-02 14:48:30 +0530

    inside sidenav some changes

--- instagram-clone/Frontend/src/components/navigation/Sidenav.jsx
+++ instagram-clone/Frontend/src/components/navigation/Sidenav.jsx
@@ -10,59 +10,55 @@ import { RxAvatar } from "react-icons/rx";
 import { FaInstagram } from "react-icons/fa";
 import { FaBars } from "react-icons/fa6";
 
-
 function Sidenav() {
   return (
     <div className="fixed border-r-white/50 border-r h-full flex items-start justify-center">
-      <div className="px-8">
-
-        <div className="flex gap-3 mt-6 items-center justify-center">
-          <FaInstagram size={"2vw"} />
+      <div className="px-8 ">
+        <div className="mt-10  ml-3">
           <img
-            className="h-9"
+            className="h-8"
             src="https://www.pngkey.com/png/full/828-8286178_mackeys-work-needs-no-elaborate-presentation-or-distracting.png"
             alt="logo"
           />
         </div>
 
-        <div className="Sidenav_buttons mt-9 flex flex-col gap-7 font-medium font-[Poppins]">
-          <button className="flex gap-4 items-center">
+        <div className="mt-10 flex flex-col gap-3 tracking-wider text-sm font-[Poppins]">
+          <button className="flex gap-3 items-center  hover:bg-slate-700 px-2 py-2 rounded-md focus:font-extrabold">
             <GoHomeFill size={"1.9vw"} />
             <span>Home</span>
           </button>
-          <button className="flex gap-4 items-center">
+          <button className="flex gap-3 items-center  hover:bg-slate-700 px-2 py-2 rounded-md focus:font-extrabold">
             <IoSearch size={"1.9vw"} />
             <span>Search</span>
           </button>
-          <button className="flex gap-4 items-center">
+          <button className="flex gap-3 items-center  hover:bg-slate-700 px-2 py-2 rounded-md focus:font-extrabold">
             <MdOutlineExplore size={"1.9vw"} />
             <span>Explore</span>
           </button>
-          <button className="flex gap-4 items-center">
+          <button className="flex gap-3 items-center  hover:bg-slate-700 px-2 py-2 rounded-md focus:font-extrabold">
             <MdSlideshow size={"1.9vw"} />
             <span>Reels</span>
           </button>
-          <button className="flex gap-4 items-center">
+          <button className="flex gap-3 items-center  hover:bg-slate-700 px-2 py-2 rounded-md focus:font-extrabold">
             <IoChatbubbleEllipsesOutline size={"1.9vw"} />
             <span>Messages</span>
           </button>
-          <button className="flex gap-4 items-center">
-            <FaRegHeart size={"1.9vw"} />
+          <button className="flex gap-3 items-center  hover:bg-slate-700 px-2 py-2 rounded-md focus:font-extrabold">
+            <FaRegHeart size={"1.7vw"} />
             <span>Notifications</span>
           </button>
-          <button className="flex gap-4 items-center">
-            <FaRegSquarePlus size={"1.9vw"} />
+          <button className="flex gap-3 items-center  hover:bg-slate-700 px-2 py-2 rounded-md focus:font-extrabold">
+            <FaRegSquarePlus size={"1.7vw"} />
             <span>Post</span>
           </button>
         </div>
-       
-        <div className="fixed bottom-4">
-           <button className="flex items-center gap-4">
-           <FaBars  size={'1.6vw'}/>
-            <span>More</span>
-           </button>
-        </div>
 
+        <div className="fixed bottom-4 text-sm font-[Poppins]">
+          <button className="flex gap-4 items-center  hover:bg-slate-700 px-2 py-2 rounded-md focus:font-extrabold">
+            <FaBars size={"1.7vw"} />
+            <span>More</span>
+          </button>
+        </div>
       </div>
     </div>
   );
